be10f351c1b2b9dffa99ffbdadd59d8945dc62f3
Vadim Novash <novash@tut.by>
2016-04-12 16:21:08 +0300

    makeup & styles created

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+        plumber = require('gulp-plumber'),
+    runSequence = require('run-sequence');
+
+gulp.task('server', function (){
+    browserSync.init({
+        port: 1313,
+        server: {
+            baseDir: './'
+        }
+    });
+});
+gulp.task('watch', function () {
+    gulp.watch([
+        'index.html',
+        'js/*.js',
+        'css/*.css'
+    ]).on('change', browserSync.reload);
+});
+//дефолтные таски
+gulp.task('default', function (callback) {
+  runSequence(['server', 'watch'],
+    callback
+  );
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+ymaps.ready(init);
+var map = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7
+});
